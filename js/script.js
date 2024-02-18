8abba70b5ad319edd771e7cba646254991eb0079
SamiulHasan-dev <samiul.199897@gmail.com>
2024-02-19 02:49:11 +0600

    price section with js

--- js/script.js
+++ js/script.js
@@ -23,38 +23,57 @@ const givenSeat = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', '
 for (let i = 0; i < givenSeat.length; i++) {
     const sitId = givenSeat[i];
     const seat = document.getElementById(sitId);
-    seat.addEventListener('click',function(){
-    seat.style.backgroundColor = '#1DD100';
-    seat.style.color = 'white';
+    seat.addEventListener('click', function () {
 
-    const available = document.getElementById('seat-available');
-    const availableIndex = available.innerText;
-    const availableNum = parseInt(availableIndex);
+        const chooseSeats = document.querySelectorAll('.selected').length;
+        if (chooseSeats < 4) {
 
-    const availableLast = availableNum - 1;
-    console.log(availableLast);
+            seat.classList.remove('available');
+            seat.classList.add('selected');
+            seat.classList.add('bg-[#1DD100]');
+            seat.classList.add('text-white');
+
+            const seatAvailable = getTextElementValueById('seat-available');
+            const seatNumber = seatAvailable - 1;
+            setTextElementValueById('seat-available', seatNumber);
+
+            const seatInfo = document.createElement('p');
+            seatInfo.innerText = sitId;
+            const seatClass = document.createElement('p');
+            seatClass.innerText = 'Economy';
+            const seatPrice = document.createElement('p');
+            seatPrice.innerText = '550';
+
+            document.getElementById('seat-info').appendChild(seatInfo);
+            document.getElementById('seat-info').appendChild(seatClass);
+            document.getElementById('seat-info').appendChild(seatPrice);
+
+            const totalPrice = getTextElementValueById('total-price');
+            const price = (totalPrice + 550).toFixed(2);
+            setTextElementValueById('total-price', price);
+
+            // const coupon = getCoupon('coupon-input');
+            
+            
+            if (chooseSeats === 3) {
+                const grandTotal = document.getElementById('grand-price');
+                const grandPrice = price - (price * 0.15);
+                grandTotal.innerText = grandPrice;
+            }
+            else {
+                const grandTotal = document.getElementById('grand-price');
+                grandTotal.innerText = price;
+            }
+
+
+        }
+        else {
+            alert("You Can't Select More Than 4 Seats");
+        }
+
+
+
 
     });
 
-    
-
-    
 }
-
-
-
-// for (let seat of givenSeat) {
-    
-//     seat.addEventListener("click", function(){
-//         btn.style.backgroundColor = 'red';
-//       })
-//     }
-
-
-
-
-
-// function getShowElement(){
-//     const hideElement = document.getElementById('success-screen');
-//     hideElement.classList.remove('hidden');
-// }
--- js/utility.js
+++ js/utility.js
@@ -9,4 +9,31 @@ function showElementById(elementId){
     elements.classList.remove('hidden');
 }
 
+function getTextElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseFloat(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+function getElementTextById(elementId) {
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
+// function click(){
+    
+// }
+
+// function getCoupon(elementId){
+//     const coupon = document.getElementById(elementId);
+//     const couponValue = coupon.value;
+//     return couponValue;
+// }
 
