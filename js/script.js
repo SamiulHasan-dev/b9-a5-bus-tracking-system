941860bbae21cc8be372b1f5298c56ce74a1710b
SamiulHasan-dev <samiul.199897@gmail.com>
2024-02-19 19:06:06 +0600

    work on seat alert

--- js/script.js
+++ js/script.js
@@ -19,6 +19,18 @@ continueButton.addEventListener('click', function () {
     window.location.reload()
 })
 
+let buttonSelected = false;
+
+function handleButtonClick(elementId) {
+    const uniqueSeat = document.getElementById(elementId);
+
+    if (!buttonSelected === uniqueSeat) {
+        buttonSelected = true;
+    } else {
+        alert('You already selected this button.');
+    }
+}
+
 const givenSeat = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4', 'E1', 'E2', 'E3', 'E4', 'F1', 'F2', 'F3', 'F4', 'G1', 'G2', 'G3', 'G4', 'H1', 'H2', 'H3', 'H4', 'I1', 'I2', 'I3', 'I4', 'J1', 'J2', 'J3', 'J4'];
 //const seats = document.querySelectorAll('.color');
 givenSeat.forEach(seatId => {
@@ -32,6 +44,8 @@ function selectSeat(elementId) {
     const seatElement = document.getElementById(elementId);
 
     const selectedSeats = document.querySelectorAll('.selected').length;
+
+    document.getElementById(elementId).addEventListener('click', handleButtonClick);
 
     if (selectedSeats < 4) {
         seatElement.classList.remove('available');
@@ -91,6 +105,8 @@ function selectSeat(elementId) {
         const showSeatQuantity = getTextElementValueById('seat-quantity');
         const seatQuantity = showSeatQuantity + 1;
         setTextElementValueById('seat-quantity', seatQuantity);
+
+        
 
     } else {
         alert('You can select maximum 4 seats.');
